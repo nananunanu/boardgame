675ac062492f0517357da8e6db7326e7c1be56d4
손건 <121307051+nananunanu@users.noreply.github.com>
2026-03-06 05:45:14 +0000

    점프속도 및 주사위 속도 설정

--- web-board-game/public/js/game.js
+++ web-board-game/public/js/game.js
@@ -194,7 +194,7 @@ const state = {
         showResult: false, // 결과 주사위를 화면에 유지할지 여부
         value: 1,          // 주사위 눈금
         frame: 0,
-        maxFrame: 60,
+        maxFrame: 40,
         yOffset: 0,        // 점프 높이
         rotation: 0        // 회전 각도
     },
@@ -283,7 +283,7 @@ function moveOneStep(playerId) { // socket "dice-result"에서 유저 움직임 
         const nextIdx = (startIdx + 1) % state.mapData.length;
         const from = state.mapData[startIdx];
         const to = state.mapData[nextIdx];
-        const frames = 60; // 점프속도 조절
+        const frames = 30; // 점프속도 조절
         let frame = 0;
 
         function animate() {
