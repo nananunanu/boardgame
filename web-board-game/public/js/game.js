2a0a885aa18b9ee0543e0e3cb78773812d059615
손건 <121307051+nananunanu@users.noreply.github.com>
2026-03-06 03:32:41 +0000

    찬스카드로 인해 이동시 해당 칸에서 이벤트가 발생하지 않는 문제 해결

--- web-board-game/public/js/game.js
+++ web-board-game/public/js/game.js
@@ -279,7 +279,7 @@ function moveOneStep(playerId) { // socket "dice-result"에서 유저 움직임 
         const nextIdx = (startIdx + 1) % state.mapData.length;
         const from = state.mapData[startIdx];
         const to = state.mapData[nextIdx];
-        const frames = 20; // 점프속도 조절
+        const frames = 30; // 점프속도 조절
         let frame = 0;
 
         function animate() {
